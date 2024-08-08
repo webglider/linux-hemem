ba78e2df4203e6e414a7c6fcf8ac7d285e220f0b
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2024-08-08 14:16:05 +0000

    add intel_get_tsx_transaction()

--- arch/x86/events/intel/ds.c
+++ arch/x86/events/intel/ds.c
@@ -1245,6 +1245,16 @@ static inline u64 intel_hsw_transaction(struct pebs_record_skl *pebs)
 	return txn;
 }
 
+static inline u64 intel_get_tsx_transaction(u64 tsx_tuning, u64 ax)
+{
+	u64 txn = (tsx_tuning & PEBS_HSW_TSX_FLAGS) >> 32;
+
+	/* For RTM XABORTs also log the abort code from AX */
+	if ((txn & PERF_TXN_TRANSACTION) && (ax & 1))
+		txn |= ((ax >> 24) & 0xff) << PERF_TXN_ABORT_SHIFT;
+	return txn;
+}
+
 static inline u64 intel_get_tsx_weight(u64 tsx_tuning)
 {
 	if (tsx_tuning) {
