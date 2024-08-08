18e2e683843f69e726000c7816a77eea67ae240c
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2024-08-08 04:12:51 +0000

    add intel_pmu_pebs_event_update_no_drain()

--- arch/x86/events/intel/ds.c
+++ arch/x86/events/intel/ds.c
@@ -1756,6 +1756,25 @@ static void intel_pmu_drain_pebs_core(struct pt_regs *iregs)
 			       setup_pebs_fixed_sample_data);
 }
 
+static void intel_pmu_pebs_event_update_no_drain(struct cpu_hw_events *cpuc, int size)
+{
+	struct perf_event *event;
+	int bit;
+
+	/*
+	 * The drain_pebs() could be called twice in a short period
+	 * for auto-reload event in pmu::read(). There are no
+	 * overflows have happened in between.
+	 * It needs to call intel_pmu_save_and_restart_reload() to
+	 * update the event->count for this case.
+	 */
+	for_each_set_bit(bit, (unsigned long *)&cpuc->pebs_enabled, size) {
+		event = cpuc->events[bit];
+		if (event->hw.flags & PERF_X86_EVENT_AUTO_RELOAD)
+			intel_pmu_save_and_restart_reload(event, 0);
+	}
+}
+
 static void intel_pmu_drain_pebs_nhm(struct pt_regs *iregs)
 {
 	struct cpu_hw_events *cpuc = this_cpu_ptr(&cpu_hw_events);
